04a10352be6889f52b420f1c3eb151cc95d8f1a0
HighDoping <highdoping@gmail.com>
2025-06-17 21:11:27 +0800

    Init ComfyUI template.

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for ComfyUI_ASSSSA."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """HighDoping"""
+__email__ = "highdoping@gmail.com"
+__version__ = "0.0.1"
+
+from .src.ComfyUI_ASSSSA.nodes import NODE_CLASS_MAPPINGS
+from .src.ComfyUI_ASSSSA.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
